e199b7890b1b5bc372a100951dd3be47821994ff
Iliyan Bachiyski <iliyan.bachiyski@limechain.tech>
2025-05-02 14:33:34 +0300

    Add: simple deploy contract script

---
+++ smart-contracts/script/DeployAuction.s.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {Auction} from "../src/Auction.sol";
+
+contract DeployAuction is Script {
+    function run() external returns (Auction) {
+        vm.startBroadcast();
+        Auction auction = new Auction();
+        vm.stopBroadcast();
+        return auction;
+    }
+}
